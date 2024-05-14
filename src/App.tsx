5d4ef72a18fe20c0708b6dd9789ec231e1d9526b
Alexis Oliveira <alexis.silvaoliveira@gmail.com>
2024-05-14 09:23:49 -0300

    Removing unused array

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,6 @@ import { SafeAreaView, StyleSheet, View } from 'react-native'
 import { Header } from './components/Header/Header'
 import { DatesTitle } from './components/DatesTitle/DatesTitle'
 
-const datas = [
-  '2024-05-11T01:08:32.825Z',
-  '2024-05-12T01:08:32.825Z',
-  '2024-05-13T01:08:32.825Z',
-  '2024-05-14T01:08:32.825Z'
-]
-
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
