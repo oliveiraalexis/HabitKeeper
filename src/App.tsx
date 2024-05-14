9aa569f597c9fbcc4a82922688dcda8c3763f470
Alexis Oliveira <alexis.silvaoliveira@gmail.com>
2024-05-14 09:16:18 -0300

    Refactoring App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet, View } from 'react-native'
 import { Header } from './components/Header/Header'
+import { DatesTitle } from './components/DatesTitle/DatesTitle'
+
+const datas = [
+  '2024-05-11T01:08:32.825Z',
+  '2024-05-12T01:08:32.825Z',
+  '2024-05-13T01:08:32.825Z',
+  '2024-05-14T01:08:32.825Z'
+]
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Header title='Hábitos'/>
+        <Header title='HÁBITOS'/>
+        <DatesTitle/>
       </View>
     </SafeAreaView>
   )
@@ -15,7 +24,8 @@ function App(): React.JSX.Element {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#161927',
-    flex: 1
+    flex: 1,
+    padding: 15
   }
 })
 
