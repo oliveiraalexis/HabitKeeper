7dac80b22b74dcbe8f675ab47e6a5e9dc6bc75a8
Alexis Oliveira <alexis.silvaoliveira@gmail.com>
2024-05-15 09:07:35 -0300

    Creating CheckButton component

---
+++ src/components/CheckButton/CheckButton.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { StyleSheet, TouchableOpacity } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+type CheckButtonProps = {
+  isCheckedDay: boolean
+}
+
+export function CheckButton({isCheckedDay}: CheckButtonProps){
+  const iconColor = isCheckedDay ? '#6676ce' : '#888888'
+  const iconName = isCheckedDay ? 'checkmark-circle-sharp' : 'close-circle-outline'
+
+  return (
+    <TouchableOpacity style={styles.container}>
+      <Icon name={iconName} color={iconColor} size={23}/>
+    </TouchableOpacity>
+  )
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    padding: 8,
+    borderRadius: 8
+  }
+})
