3cac7a00f83758b93eddedb91280a496ecc48bd6
Alexis Oliveira <alexis.silvaoliveira@gmail.com>
2024-05-15 09:07:47 -0300

    Importing CondensedHabit component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { SafeAreaView, StyleSheet, View } from 'react-native'
 import { Header } from './components/Header/Header'
 import { DatesTitle } from './components/DatesTitle/DatesTitle'
+import { CondensedHabit } from './components/CondensedHabit/CondensedHabit'
 
 function App(): React.JSX.Element {
   return (
@@ -9,6 +10,8 @@ function App(): React.JSX.Element {
       <View>
         <Header title='HÁBITOS'/>
         <DatesTitle/>
+        <CondensedHabit habit={{title: 'Primeiro hábito', last4Days: [false, true, true, true]}}/>
+        <CondensedHabit habit={{title: 'Primeiro hábito', last4Days: [false, true, true, true]}}/>
       </View>
     </SafeAreaView>
   )
@@ -18,7 +21,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#161927',
     flex: 1,
-    padding: 15
+    padding: 10
   }
 })
 
