d6cebe5b9eb40133dc9bc7fdfc6a8100947c87c2
Alexis Oliveira <alexis.silvaoliveira@gmail.com>
2024-05-17 00:03:47 -0300

    Adding HabitDetailScreen components

--- src/pages/HabitDetailScreen.tsx
+++ src/pages/HabitDetailScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
 import { Calendar } from '../components/Calendar/Calendar'
 import { Header } from '../components/Header/Header'
 
@@ -8,20 +8,24 @@ export function HabitDetailScreen(){
   return (
     <View>
       <Header title='Primeiro hábitovd fvfdvfd' isDetailScreen={true}/>
-      <Calendar/>
+      <View style={styles.contentContainer}>
+        <Calendar/>
+        <Text style={styles.text}>Total de dias marcados: 0</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   contentContainer: {
-    flex: 1,
     paddingHorizontal: 20,
     marginBottom: 30
   },
-  buttons:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 20
-}
+  text:{
+    alignSelf: 'center',
+    margin: 30,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 15
+  }
 })
