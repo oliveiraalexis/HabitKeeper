8b4fe265f2a28050e472310d8886029f62dd2145
Alexis Oliveira <alexis.silvaoliveira@gmail.com>
2024-06-29 17:23:55 -0300

    Removing unused type

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import { Routes } from './routes/Routes'
 
-export type RootStackParamList = {
-  HabitListScreen: undefined,
-  HabitDetailScreen: {
-    habitId: string
-  }
-}
-
 function App(): React.JSX.Element {
 
   return (
