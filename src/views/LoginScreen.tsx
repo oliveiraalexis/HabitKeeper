e2d37824abe3a071948384419b1f0501b4f7e420
Alexis Oliveira <alexis.silvaoliveira@gmail.com>
2024-07-12 01:31:43 -0300

    Improving LoginScreen component

--- src/views/LoginScreen.tsx
+++ src/views/LoginScreen.tsx
@@ -1,19 +1,35 @@
-import { SafeAreaView, StyleSheet, View } from "react-native"
+import { Alert, SafeAreaView, StyleSheet, View } from "react-native"
 import { TextInput } from "../components/TextInput/TextInput"
 import { Button } from "../components/Button/Button"
-import Icon from 'react-native-vector-icons/Ionicons'
 import { Header } from "../components/Header/Header"
+import { useUser } from "../controllers/useUser"
+import { useState } from "react"
+import { NativeStackScreenProps } from "@react-navigation/native-stack"
+import { RootStackParamList } from "../routes/Routes"
 
-export function LoginScreen() {
+type LoginScreenProps = NativeStackScreenProps<RootStackParamList, 'LoginScreen'>;
+
+export function LoginScreen({navigation}: LoginScreenProps) {
+
+  const { loginUser } = useUser()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function login(){
+    const result = await loginUser(username, password)
+    if (result && result.username){
+      navigation.navigate('HabitListScreen',  {userId: result._id})
+    }
+  }
   
   return (
     <SafeAreaView style={styles.container}>
       <Header title="HABIT KEEPER" isLoginScreen />
       <View>
-        <TextInput placeholder='Digite seu login'/>
-        <TextInput placeholder='Digite sua senha' secureTextEntry/>
+        <TextInput placeholder='Digite seu login' func={setUsername}/>
+        <TextInput placeholder='Digite sua senha' func={setPassword} secureTextEntry/>
         <View style={{marginTop: 20}}>
-          <Button text='ENTRAR' background='#6676ce'height={50}/>
+          <Button text='ENTRAR' background='#6676ce'height={50} onPress={login}/>
         </View>
       </View>
     </SafeAreaView>
