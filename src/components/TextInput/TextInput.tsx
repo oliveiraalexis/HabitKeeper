42d19b8392e9ec8c5310622d32b8d02df3ee79e4
Alexis Oliveira <alexis.silvaoliveira@gmail.com>
2024-07-11 22:19:37 -0300

    Removing unused import

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import { StyleSheet, TextInputProps, TextInput as TextInputRN } from "react-native"
 
 export function TextInput(props: TextInputProps){
